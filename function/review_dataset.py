Fix attention mask. Ready to train.

function/review_dataset.py
@@ -29,6 +29,8 @@ class ReviewDataset(Dataset):
         item_review_emb = torch.from_numpy(np.array(item_review_data["SplitReview_emb"].tolist()))
         pad_user_emb = torch.zeros(self.args["max_review_user"], user_review_emb.size(1), user_review_emb.size(2))
         pad_item_emb = torch.zeros(self.args["max_review_item"], item_review_emb.size(1), item_review_emb.size(2))
+        k_user_review_mask = torch.zeros(self.args["max_review_user"], dtype=torch.bool)
+        k_item_review_mask = torch.zeros(self.args["max_review_item"], dtype=torch.bool)
 
         user_lda_groups = torch.from_numpy(np.array(user_review_data["LDA_group"].tolist()))
         item_lda_groups = torch.from_numpy(np.array(item_review_data["LDA_group"].tolist()))
@@ -40,11 +42,13 @@ class ReviewDataset(Dataset):
             pad_user_emb = user_review_emb[:self.args["max_review_user"], :, :]
         else:
             pad_user_emb[:user_review_emb.size(0), :, :] = user_review_emb
+            k_user_review_mask[user_review_emb.size(0):] = True
 
         if item_review_emb.size(0) > self.args["max_review_item"]:
             pad_item_emb = item_review_emb[:self.args["max_review_item"], :, :]
         else:
             pad_item_emb[:item_review_emb.size(0), :, :] = item_review_emb
+            k_item_review_mask[item_review_emb.size(0):] = True
 
         if user_lda_groups.size(0) > self.args["max_review_user"]:
             pad_user_lda = user_lda_groups[:self.args["max_review_user"], :]
@@ -59,7 +63,10 @@ class ReviewDataset(Dataset):
         user_mf_emb =  torch.from_numpy(self.user_mf_df[self.user_mf_df["UserID"]==userId]["MF_emb"].values[0])
         item_mf_emb =  torch.from_numpy(self.item_mf_df[self.item_mf_df["AppID"]==itemId]["MF_emb"].values[0])
 
-        return pad_user_emb, pad_item_emb, pad_user_lda, pad_item_lda, user_mf_emb, item_mf_emb ,y
+        user_review_mask = torch.logical_or(k_user_review_mask.unsqueeze(dim=-1), k_user_review_mask.unsqueeze(dim=0))
+        item_review_mask = torch.logical_or(k_item_review_mask.unsqueeze(dim=-1), k_item_review_mask.unsqueeze(dim=0))
+
+        return pad_user_emb, pad_item_emb, user_review_mask, item_review_mask, pad_user_lda, pad_item_lda, user_mf_emb, item_mf_emb , y
 
     def __len__(self):
         return len(self.review_df)
@@ -124,7 +131,6 @@ class ReviewDataseStage1(ReviewDataset):
             pad_item_y = item_y[:self.args["max_review_item"]]
         else:
             pad_item_y[:item_y.size(0)] = item_y
-
 
         user_mf_emb = torch.from_numpy(self.user_mf_df[self.user_mf_df["UserID"]==userId]["MF_emb"].values[0])
         item_mf_emb = torch.from_numpy(self.item_mf_df[self.item_mf_df["AppID"]==itemId]["MF_emb"].values[0])

function/train.py
@@ -32,9 +32,9 @@ def train_model(args, train_loader, val_loader, user_network, item_network, co_a
         for batch in tqdm(train_loader):
 
             # Exacute models
-            user_review_emb, item_review_emb, user_lda_groups, item_lda_groups, user_mf_emb, item_mf_emb, labels = batch
-            user_logits = user_network(user_review_emb.to(args["device"]), user_lda_groups.to(args["device"]))
-            item_logits = item_network(item_review_emb.to(args["device"]), item_lda_groups.to(args["device"]))
+            user_review_emb, item_review_emb, user_review_mask, item_review_mask, user_lda_groups, item_lda_groups, user_mf_emb, item_mf_emb, labels = batch
+            user_logits = user_network(user_review_emb.to(args["device"]), user_review_mask.to(args["device"]), user_lda_groups.to(args["device"]))
+            item_logits = item_network(item_review_emb.to(args["device"]), item_review_mask.to(args["device"]), item_lda_groups.to(args["device"]))
             weighted_user_logits,  weighted_item_logits = co_attention(user_logits, item_logits)
             user_feature = torch.cat((weighted_user_logits, user_mf_emb.to(args["device"])), dim=1)
             item_feature = torch.cat((weighted_item_logits, item_mf_emb.to(args["device"])), dim=1)
@@ -105,9 +105,9 @@ def train_model(args, train_loader, val_loader, user_network, item_network, co_a
             with torch.no_grad():
 
                 # Exacute models 
-                user_review_emb, item_review_emb, user_lda_groups, item_lda_groups, user_mf_emb, item_mf_emb, labels = batch
-                user_logits = user_network(user_review_emb.to(args["device"]), user_lda_groups.to(args["device"]))
-                item_logits = item_network(item_review_emb.to(args["device"]), item_lda_groups.to(args["device"]))
+                user_review_emb, item_review_emb, user_review_mask, item_review_mask, user_lda_groups, item_lda_groups, user_mf_emb, item_mf_emb, labels = batch
+                user_logits = user_network(user_review_emb.to(args["device"]), user_review_mask.to(args["device"]), user_lda_groups.to(args["device"]))
+                item_logits = item_network(item_review_emb.to(args["device"]), item_review_mask.to(args["device"]),item_lda_groups.to(args["device"]))
                 weighted_user_logits,  weighted_item_logits = co_attention(user_logits, item_logits)
                 user_feature = torch.cat((weighted_user_logits, user_mf_emb.to(args["device"])), dim=1)
                 item_feature = torch.cat((weighted_item_logits, item_mf_emb.to(args["device"])), dim=1)

function/train_stage2.py
@@ -51,16 +51,13 @@ def train_stage2_model(args,
         for batch in tqdm(train_loader):
 
             # Exacute models
-            user_review_emb, item_review_emb, user_lda_groups, item_lda_groups, user_mf_emb, item_mf_emb, labels = batch
+            user_review_emb, item_review_emb, user_review_mask, item_review_mask, user_lda_groups, item_lda_groups, user_mf_emb, item_mf_emb, labels = batch
             u_batch_size, i_batch_size = len(user_review_emb), len(item_review_emb)
-            user_logits = user_network_stage1(user_review_emb.to(args["device"]), user_lda_groups.to(args["device"]))
-            item_logits = item_network_stage1(item_review_emb.to(args["device"]), item_lda_groups.to(args["device"]))
-            urf, urf_1 = user_review_network(user_logits, u_batch_size)
-            irf, irf_1 = item_review_network(item_logits, u_batch_size)
-            urf = bp_gate.apply(urf)
-            urf_1 = bp_gate.apply(urf_1)
-            irf = bp_gate.apply(irf)
-            irf_1 = bp_gate.apply(irf_1)
+            user_arv = user_network_stage1(user_review_emb.to(args["device"]), user_lda_groups.to(args["device"]))
+            item_arv = item_network_stage1(item_review_emb.to(args["device"]), item_lda_groups.to(args["device"]))
+            urf, urf_1 = user_review_network(user_arv, user_review_mask.to(args["device"]), u_batch_size)
+            irf, irf_1 = item_review_network(item_arv, item_review_mask.to(args["device"]), i_batch_size)
+            urf, urf_1, irf, irf_1 = bp_gate.apply(urf), bp_gate.apply(urf_1), bp_gate.apply(irf), bp_gate.apply(irf_1)
             w_urf, w_urf_1, w_urf_2, w_urf_3, w_irf, w_irf_1, w_irf_2, w_irf_3 = co_attentions(urf, irf, urf, urf_1, urf_1, irf, irf_1, irf_1)
             
             user_feature = torch.cat((w_urf, user_mf_emb.to(args["device"])), dim=1)
@@ -90,7 +87,7 @@ def train_stage2_model(args,
             loss.backward()
 
             # Clip the gradient norms for stable training.
-            grad_norm = nn.utils.clip_grad_norm_(models_param, max_norm=10)
+            grad_norm = nn.utils.clip_grad_norm_(models_param, max_norm=1)
 
             # Update the parameters with computed gradients.
             optimizer.step()
@@ -150,10 +147,10 @@ def train_stage2_model(args,
                 # Exacute models       
                 user_review_emb, item_review_emb, user_lda_groups, item_lda_groups, user_mf_emb, item_mf_emb, labels = batch
                 u_batch_size, i_batch_size = len(user_review_emb), len(item_review_emb)
-                user_logits = user_network_stage1(user_review_emb.to(args["device"]), user_lda_groups.to(args["device"]))
-                item_logits = item_network_stage1(item_review_emb.to(args["device"]), item_lda_groups.to(args["device"]))
-                urf = user_review_network(user_logits, u_batch_size)
-                irf = item_review_network(item_logits, i_batch_size)
+                user_arv = user_network_stage1(user_review_emb.to(args["device"]), user_lda_groups.to(args["device"]))
+                item_arv = item_network_stage1(item_review_emb.to(args["device"]), item_lda_groups.to(args["device"]))
+                urf = user_review_network(user_arv, u_batch_size)
+                irf = item_review_network(item_arv, i_batch_size)
                 w_urf, w_irf = co_attentions(urf, irf)
                 user_feature = torch.cat((w_urf, user_mf_emb.to(args["device"])), dim=1)
                 item_feature = torch.cat((w_irf, item_mf_emb.to(args["device"])), dim=1)

model/hian.py
@@ -1,6 +1,9 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch
+from torch.nn.utils.rnn import pad_sequence
+from .attention_utils import Multihead_Cross_attention
+
 
 class HianModel(nn.Module):
     """
@@ -25,7 +28,7 @@ class HianModel(nn.Module):
                             |
                             |aspect attention
                             v
-                    x: 32*R, D  (B*R, D)
+                    x: 32, R, D  (B, R, D)
                             |
                             |review_network
                             v  
@@ -42,7 +45,7 @@ class HianModel(nn.Module):
     Sentence Emb:           torch.Size([32*50, 10, 512])
     Weighted Sentence Emb:  torch.Size([32*50, 10, 512])
     Aspect Emb:             torch.Size([32*50, 6, 512])
-    Aspect Review Emb:      torch.Size([32*50, 512])
+    Aspect Review Emb:      torch.Size([32, 50, 512])
     Item Reiew feature:     torch.Size([32, 512])
     Item Emb:               torch.Size([32, 512]) (after co-attention) 
     """
@@ -58,7 +61,7 @@ class HianModel(nn.Module):
             nn.ReLU(),
             nn.Dropout(p=0.2),
         )
-        self.word_attention = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
+        self.word_attention = nn.MultiheadAttention(512, num_heads=2, batch_first=True)
         
         # Sentence-Level Network
         self.sent_pad_size = int((self.args["sentence_cnn_ksize"]-1)/2)
@@ -67,23 +70,22 @@ class HianModel(nn.Module):
             nn.ReLU(),
             nn.Dropout(p=0.2),
         )
-        self.sentence_attention = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
+        self.sent_cross_attention = Multihead_Cross_attention(512, 512, 512, num_heads=2)
 
         # Aspect-Level Network
         self.lda_group_num = self.args["lda_group_num"]
-        self.aspect_attention = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
+        self.aspect_cross_attention = Multihead_Cross_attention(512, 512, 512, num_heads=2)
 
         
         # Review-Level Network
-        self.review_attention = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
+        self.review_cross_attention = Multihead_Cross_attention(512, 512, 512, num_heads=2)
 
     def word_level_network(self, x, word_cnn, word_attention):
         x = torch.permute(x, (0, 2, 1))
         x = F.pad(x, (self.word_pad_size, self.word_pad_size), "constant", 0) # same to keras: padding = same
         x = word_cnn(x)
         x = torch.permute(x, [0, 2, 1])
-        attn_output, att_weight = word_attention(x, x, x, need_weights=True)
-        x = x * attn_output[0] 
+        x, att_weight = word_attention(x, x, x, need_weights=True)
         x = self.word_weighted_sum(x, self.args["max_sentence"])
         return x
         
@@ -98,6 +100,9 @@ class HianModel(nn.Module):
         return sentence_tensor
     
     def sentence_level_network(self, x, sent_cnn, sent_attention, lda_groups):
+        """
+        Be careful that we're using self defined attention not torch.nn.MultiheadAttention
+        """
         x = torch.permute(x, [0, 2, 1])
         x = F.pad(x, (self.sent_pad_size, self.sent_pad_size), "constant", 0) # same to keras: padding = same
         x = sent_cnn(x)
@@ -105,22 +110,21 @@ class HianModel(nn.Module):
         # Mask generate
         k_sent_mask = (lda_groups == False).reshape(-1, lda_groups.size(2))
         sent_mask = torch.logical_or(k_sent_mask.unsqueeze(dim=-1), k_sent_mask.unsqueeze(dim=1))
-        # attn_output, sent_att_weight = sent_attention(x, x, x, attn_mask=sent_mask, need_weights=True)
-        attn_output, sent_att_weight = sent_attention(x, x, x, need_weights=True)
-        attn_output = torch.nan_to_num(attn_output, nan=0)
-        x = x * attn_output[0]
+        x, att_weight = sent_attention(x, x, mask=~sent_mask.to(self.args["device"]))
+        # x, sent_att_weight = sent_attention(x, x, x, attn_mask=sent_mask, need_weights=True)
+        # x, sent_att_weight = sent_attention(x, x, x, need_weights=True)
+        # x = torch.nan_to_num(x, nan=0)
         return x 
     
     def aspect_level_network(self, x, lda_groups, aspect_attention):
+        """
+        Be careful that we're using self defined attention not torch.nn.MultiheadAttention
+        """
         x, aspect_att_mask = self.get_aspect_emb_from_sent(x, lda_groups, self.lda_group_num)
-        x = torch.nan_to_num(x, nan=1e-10) 
-        
-        # attn_output, att_weight = aspect_attention(x, x, x, attn_mask=aspect_att_mask.to(self.args["device"]), need_weights=True)
-        attn_output, att_weight = aspect_attention(x, x, x, need_weights=True)
-        attn_output = torch.nan_to_num(attn_output, nan=0)
-        x = x * attn_output[0]
-        x = torch.nan_to_num(x, nan=0)
-        x = torch.sum(x, dim=1) #weighted sum
+        # x, att_weight = aspect_attention(x, x, x, attn_mask=aspect_att_mask.to(self.args["device"]), need_weights=True)
+        x, att_weight = aspect_attention(x, x, mask=~aspect_att_mask.to(self.args["device"])) # the input mask should be reversed compare to nn.MultiheadAttention
+        x = torch.sum(x, dim=1)
+
         return x
     
     def get_aspect_emb_from_sent(self, input_tensor, lda_groups, group_num):
@@ -135,7 +139,7 @@ class HianModel(nn.Module):
             mask = lda_groups == group
             mask_sum = torch.sum(mask, dim=1)
             mask_sum[mask_sum == 0] = 1
-            group_tensor = torch.where(mask , input_tensor, 0.0)
+            group_tensor = torch.where(mask , input_tensor, 0.)
             group_tensor = torch.sum(group_tensor, dim = 1)
             group_tensor = group_tensor/mask_sum
             group_tensor_list.append(group_tensor)
@@ -150,28 +154,25 @@ class HianModel(nn.Module):
         k_aspect_att_mask = ~k_aspect_att_mask
         aspect_att_mask = torch.logical_or(k_aspect_att_mask.unsqueeze(dim=-1), k_aspect_att_mask.unsqueeze(dim=1))
         aspect_review_tensor = torch.stack(group_tensor_list, dim=1)
-        
-        # print("lda_groups: ",lda_groups.squeeze(dim=-1)[0])
-        # print("k_aspect_att_mask: ", k_aspect_att_mask.shape)
-        # print("aspect_review_tensor: ",aspect_review_tensor[0])
-        # print("aspect_att_mask: ", aspect_att_mask.size())
 
         return aspect_review_tensor, aspect_att_mask
 
-    def review_level_network(self, x, review_attetion, *, batch_size):
-        attn_output = review_attetion(x, x, x, need_weights=False)
-        x = x * attn_output[0]
-        x = x.reshape(batch_size, -1, x.size(1))
-        return x 
-    
+    def review_level_network(self, x, review_mask, review_attention):
+        """
+        Be careful that we're using self defined attention not torch.nn.MultiheadAttention
+        """
+        x, _ = review_attention(x, x, mask=review_mask)
 
-    def forward(self, x, lda_groups):
+        return x 
+
+    def forward(self, x, review_mask, lda_groups):
 
         batch_size, num_review, num_words, word_dim = x.shape
         x = x.reshape(-1, x.size(2), x.size(3))
         x = self.word_level_network(x, self.word_cnn_network, self.word_attention)
-        x = self.sentence_level_network(x, self.sentence_cnn_network, self.sentence_attention, lda_groups)
-        x = self.aspect_level_network(x, lda_groups, self.aspect_attention)
-        x = self.review_level_network(x, self.review_attention, batch_size=batch_size)
+        x = self.sentence_level_network(x, self.sentence_cnn_network, self.sent_cross_attention, lda_groups)
+        x = self.aspect_level_network(x, lda_groups, self.aspect_cross_attention)
+        x = x.reshape(batch_size, num_review, -1)
+        x = self.review_level_network(x, review_mask, self.review_cross_attention)
 
         return x

model/hian_cl_stage1.py
@@ -1,8 +1,7 @@
 from .hian import HianModel
 from .bp_gate import BackPropagationGate
 import torch.nn as nn
-import torch.nn.functional as F
-import torch
+from .attention_utils import Multihead_Cross_attention
 
 class HianCollabStage1(HianModel):
     def __init__(self, args):
@@ -14,12 +13,16 @@ class HianCollabStage1(HianModel):
             nn.ReLU(),
             nn.Dropout(p=0.2),
         )
-        self.sentence_attention_1 = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
+        # self.sentence_attention_1 = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
+        self.sent_cross_attention_1 = Multihead_Cross_attention(512, 512, 512, num_heads=2)
 
         # Aspect-Level Network
-        self.aspect_attention_1 = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
-        self.aspect_attention_2 = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
-        self.aspect_attention_3 = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
+        # self.aspect_attention_1 = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
+        # self.aspect_attention_2 = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
+        # self.aspect_attention_3 = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
+        self.aspect_cross_attention_1 = Multihead_Cross_attention(512, 512, 512, num_heads=2)
+        self.aspect_cross_attention_2 = Multihead_Cross_attention(512, 512, 512, num_heads=2)
+        self.aspect_cross_attention_3 = Multihead_Cross_attention(512, 512, 512, num_heads=2)
 
     def forward(self, x, lda_groups):
         
@@ -30,18 +33,18 @@ class HianCollabStage1(HianModel):
         x_s = BackPropagationGate.apply(x_s)
         
         # Sentence-Level Network
-        x_as = self.sentence_level_network(x_s, self.sentence_cnn_network, self.sentence_attention, lda_groups)
+        x_as = self.sentence_level_network(x_s, self.sentence_cnn_network, self.sent_cross_attention, lda_groups)
         x_as = BackPropagationGate.apply(x_as)
         if self.training:
-            x_as_1 = self.sentence_level_network(x_s, self.sentence_cnn_network_1, self.sentence_attention_1, lda_groups)
+            x_as_1 = self.sentence_level_network(x_s, self.sentence_cnn_network_1, self.sent_cross_attention_1, lda_groups)
             x_as_1 = BackPropagationGate.apply(x_as_1)
 
         # Aspect-Level Network
-        x_ar = self.aspect_level_network(x_as, lda_groups, self.aspect_attention)
+        x_ar = self.aspect_level_network(x_as, lda_groups, self.aspect_cross_attention)
         if self.training:
-            x_ar_1 = self.aspect_level_network(x_as, lda_groups, self.aspect_attention_1)
-            x_ar_2 = self.aspect_level_network(x_as_1, lda_groups, self.aspect_attention_2)
-            x_ar_3 = self.aspect_level_network(x_as_1, lda_groups, self.aspect_attention_3)
+            x_ar_1 = self.aspect_level_network(x_as, lda_groups, self.aspect_cross_attention_1)
+            x_ar_2 = self.aspect_level_network(x_as_1, lda_groups, self.aspect_cross_attention_1)
+            x_ar_3 = self.aspect_level_network(x_as_1, lda_groups, self.aspect_cross_attention_1)
             return x_ar, x_ar_1, x_ar_2, x_ar_3
 
         return x_ar

model/review_net_stage2.py
@@ -1,21 +1,23 @@
-import torch.nn as nn
-import torch.nn.functional as F
 from .hian import HianModel
 from .bp_gate import BackPropagationGate
+from .attention_utils import Multihead_Cross_attention
 class ReviewNetworkStage2(HianModel):
     def __init__(self, args):
         super().__init__(args)
 
         # Review-Level Network
-        self.review_attention_1 = nn.MultiheadAttention(512, num_heads=1, batch_first=True)
+        self.review_cross_attention_1 = Multihead_Cross_attention(512, 512, 512, num_heads=2)
 
-    def forward(self, x, batch_size):
+    def forward(self, x, review_mask, batch_size):
+        
+        x = x.reshape(batch_size, -1, x.size(1))
+        
         #Review-Level Network
-        x_rf = self.review_level_network(x, self.review_attention, batch_size=batch_size)
+        x_rf = self.review_level_network(x, review_mask, self.review_cross_attention)
         x_rf = BackPropagationGate.apply(x_rf)
 
         if self.training:
-            x_rf_1 = self.review_level_network(x, self.review_attention_1, batch_size=batch_size)
+            x_rf_1 = self.review_level_network(x, review_mask, self.review_cross_attention_1)
             x_rf_1 = BackPropagationGate.apply(x_rf_1)
             return x_rf, x_rf_1
         

run.py
@@ -35,7 +35,7 @@ def main(**args):
         fc_layer = FcLayer().to(device)
 
         # Loss criteria
-        criterion = nn.MSELoss()
+        criterion = nn.BCELoss()
         params = list(user_network_model.parameters()) + list(item_network_model.parameters()) + list(co_attention.parameters()) + list(fc_layer.parameters())
         optimizer = torch.optim.Adam(params, lr=1e-3, weight_decay=1e-4)
 
@@ -87,9 +87,9 @@ def main(**args):
         bp_gate = BackPropagationGate()
 
         # Loss criteria
-        user_criterion_stage1 = nn.CrossEntropyLoss()
-        item_criterion_stage1 = nn.CrossEntropyLoss()
-        criterion_stage2 = nn.CrossEntropyLoss()
+        user_criterion_stage1 = nn.BCELoss()
+        item_criterion_stage1 = nn.BCELoss()
+        criterion_stage2 = nn.BCELoss()
 
         # Parameters
         user_params_stage1 = (list(user_network_stage1.parameters()) + 
@@ -262,7 +262,7 @@ if __name__ == "__main__":
         "sentence_cnn_ksize" : 3,   # odd number 
         "epoch" : 40,
         "batch_size": 32,
-        "collab_learning": True,
+        "collab_learning": False,
         "epoch_stage1" : 10,
         "epoch_stage2" : 10,
         "trade_off_stage1": 0.3, 